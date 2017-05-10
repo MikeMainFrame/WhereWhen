920ed9f9ac845c8ebb9b9c0665e6bc404a88017f
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-10 13:17:44 +0100

    Create wwTransaction.php

---
+++ wwTransaction.php
@@ -0,0 +1,14 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1); 
+  $postdata = file_get_contents("php://input"); 
+  $zFileContents = file_get_contents("gs://XML/ww.xml");
+  $dom = new DOMDocument; 
+  $dom->loadXML($zFileContents); 
+  $root = $dom->documentElement;                    
+  $row = $dom->createDocumentFragment();   
+  $row->appendXML($postdata);
+  $root->appendChild($row);
+  file_put_contents("gs://XML/ww.xml", $dom->saveXML());
+  echo "good";
+?>
