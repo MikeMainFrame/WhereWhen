94d456cc0ae3518f1b3f242ce8691a4df34108f9
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-10 13:19:13 +0100

    Update wwTransaction.php

--- wwTransaction.php
+++ wwTransaction.php
@@ -2,13 +2,13 @@
   error_reporting(E_ALL);
   ini_set("display_errors", 1); 
   $postdata = file_get_contents("php://input"); 
-  $zFileContents = file_get_contents("gs://XML/wwTask.xml");
+  $zFileContents = file_get_contents("gs://XML/wwtask.xml");
   $dom = new DOMDocument; 
   $dom->loadXML($zFileContents); 
   $root = $dom->documentElement;                    
   $row = $dom->createDocumentFragment();   
   $row->appendXML($postdata);
   $root->appendChild($row);
-  file_put_contents("gs://XML/wwTask.xml", $dom->saveXML());
+  file_put_contents("gs://XML/wwtask.xml", $dom->saveXML());
   echo "good";
 ?>
