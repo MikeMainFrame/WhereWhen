a016dfdd4c1d670d04bf2365f109c7c00b519569
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-12 09:48:14 +0100

    Update wwMonitor.js

--- staticdata/wwMonitor.js
+++ staticdata/wwMonitor.js
@@ -1,5 +1,6 @@
 var id, map, lat, lng, wwObject = {id: 0, lat: 0, lng: 0, duration: 0, timestamp: 1};
 document.getElementById('wwTask').textContent ="MikeWasHere";
+
 function showOnMap(latLng) { 
   var zCenter = {lat: latLng.coords.latitude, lng: latLng.coords.longitude};
   var map = new google.maps.Map(document.getElementById('wwMap'), { zoom: 4, center: zCenter });
