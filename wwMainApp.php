fbdeb211c59cdd5184549e799d80dc4f044d9ea5
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-15 08:27:33 +0100

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -5,7 +5,7 @@
     <title>WhereWhen :: by Michael Kenneth Rasch :: TRITICUM</title>
     <style>
       html, body, #wwMap, #wwTask {
-        height: 50%;
+        height: 100%;
         margin: 0px;
         padding: 0px
       }
@@ -36,7 +36,6 @@
     <button type="button" value="Start">Start</button>    
   </form>
   <div id="wwMap">wwMap</div>
-  <div id="wwTask">
   <?php
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
@@ -57,7 +56,6 @@
     echo "</div>";
     // echo $dom->saveXML();
   ?>
-  </div>
 </body>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVV112FdOVJA4U5BSwImhPzEI73mgYUjQ" async defer></script>
 <script type="text/javascript" src="staticdata/wwMonitor.js"></script>
