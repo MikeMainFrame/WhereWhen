2f4ceb0d9be457216550337935e4cbf2fbb93214
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-15 07:37:42 +0100

    Update wwMonitor.js

--- staticdata/wwMonitor.js
+++ staticdata/wwMonitor.js
@@ -4,8 +4,13 @@ document.getElementById('wwTask').textContent ="Mike Was Here";
 
 function gMap(latLng) { 
   const zWhere = {lat: latLng.coords.latitude, lng: latLng.coords.longitude};  
-  var zMap = new google.maps.Map(document.getElementById('wwMap'), {center: zWhere, zoom: 4}); 
-  var zMarker = new google.maps.Marker({position: zWhere, map: zMap});
+  var zMap = new google.maps.Map(document.getElementById('wwMap'), {
+      center: zWhere, zoom: 15, icon: {
+        path: google.maps.SymbolPath.CIRCLE, scale: 10
+      }
+    }); 
+  var zMarker = new google.maps.Marker({position: zWhere, map: zMap, animation: google.maps.Animation.DROP});
+  
 }
 function GetGPSCoords(latLng) {
    document.getElementById('geolatlng').textContent = latLng.coords.latitude + ", " + latLng.coords.longitude;
