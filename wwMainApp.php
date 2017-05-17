0ba29a92a483d21ff900a91477dfaffc7e581e22
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-17 07:52:32 +0100

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>WhereWhen :: by Michael Kenneth Rasch :: TRITICUM</title>
     <style>
-      html, body, #wwMap, #wwTask {
+      html, body, #wwMap {
         height: 100%;
         margin: 0px;
         padding: 0px
@@ -15,7 +15,7 @@
   <div id="wwMap">wwMap</div>
   <svg viewBox="0 0 600 300" style="width: 100%; font-family: 'Racing Sans One'"  id="zTasks" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
     <defs>
-      <link xmlns="http://www.w3.org/1999/xhtml" href="http://fonts.googleapis.com/css?family=Racing+Sans+One|Six+Caps" type="text/css" rel="stylesheet" />
+      <link xmlns="http://www.w3.org/1999/xhtml" href="https://fonts.googleapis.com/css?family=Racing+Sans+One|Six+Caps" type="text/css" rel="stylesheet" />
     </defs>
   <?php
     error_reporting(E_ALL);
@@ -32,7 +32,7 @@
     foreach ($tasks as $task) {      
       echo "<rect x='000' y='" . $y . "' width='600' height='50' rx='5' fill='#000060' />";
       echo "<rect x='020' y='" . ($y + 10) . "' width='40' height='30' rx='5' fill='#0000f0' />";
-      echo "<text x='030' y='" . ($y + 10) . "'>" . $task->getAttribute('duration') . "</text>";
+      echo "<text x='030' y='" . ($y + 25) . "'>" . $task->getAttribute('duration') . "</text>";
       $y = $y + 50;
       //echo "<span class='taskItems'>" . $task->getAttribute('timestamp') . "</span>";
       //echo "<span class='taskItems'>" . $task->getAttribute('lat') . "</span>";
@@ -43,7 +43,6 @@
     // echo $dom->saveXML();
   ?>
   </svg>
-<div id="wwTask" />
 </body>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVV112FdOVJA4U5BSwImhPzEI73mgYUjQ" async defer></script>
 <script type="text/javascript" src="staticdata/wwMonitor.js"></script>
