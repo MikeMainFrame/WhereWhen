173b0a2f78ef63e1a20fb1a254814c43087857cd
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-22 11:16:43 +0200

    Update wwGetTasks.php

--- wwGetTasks.php
+++ wwGetTasks.php
@@ -2,8 +2,7 @@
   error_reporting(E_ALL);
   ini_set("display_errors", 1); 
   $zFileContents = file_get_contents("gs://wherewhen/XML/wwtask.xml");
-  echo $zFileContents;
   $dom = new DOMDocument;
   $dom->loadXML($zFileContents);
   $dom->saveXML();
-  ?>
+?>
