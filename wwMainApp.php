a83d1335bf728c6f38524c62b648587191926d58
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-24 11:59:36 +0100

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -12,15 +12,16 @@
   <svg viewBox="0 0 600 300" style="width: 100%; font-family: 'Racing Sans One'"  
        id="zTasks" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
     <defs>
-      <link xmlns="http://www.w3.org/1999/xhtml" xlink:href="https://fonts.googleapis.com/css?family=Racing+Sans+One|Six+Caps" type="text/css" rel="stylesheet" />
+      <link xmlns="http://www.w3.org/1999/xhtml" 
+            xlink:href="https://fonts.googleapis.com/css?family=Racing+Sans+One" type="text/css" rel="stylesheet" />
     </defs>
     <g text-anchor="right" font-family="Racing Sans One" >
       <rect x=0 y=0 width=600 height=50 fill="rgba(59, 120, 231,1)" />
       <text  x=10 y=30 text-anchor="left"  fill="#fff">ID</text>
       <text  x=60 y=30 text-anchor="right" fill="#fff">Duration seconds</text>
-      <text x=200 y=30 text-anchor="right" fill="#fff">Timestamp</text>
-      <text x=300 y=30 text-anchor="right" fill="#fff">Latitude</text>
-      <text x=400 y=30 text-anchor="right" fill="#fff">Lontitude</text>
+      <text x=300 y=30 text-anchor="right" fill="#fff">Timestamp</text>
+      <text x=450 y=30 text-anchor="right" fill="#fff">Latitude</text>
+      <text x=600 y=30 text-anchor="right" fill="#fff">Lontitude</text>
     </g>
     <g  id="wwTasks" text-anchor="right" fill="#000" font-family="Racing Sans One" >
     </g>
