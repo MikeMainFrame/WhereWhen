d39b3ae7313074dd20d6fcc8ccf2be287afd4aa5
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-24 12:29:01 +0100

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -19,9 +19,9 @@
       <rect x=0 y=0 width=600 height=50 fill="rgba(59, 120, 231,1)" rx=5 />
       <text  x=10 y=30 text-anchor="left"  fill="#fff">ID</text>
       <text  x=60 y=30 text-anchor="right" fill="#fff">Duration seconds</text>
-      <text x=240 y=30 text-anchor="right" fill="#fff">Timestamp</text>
-      <text x=390 y=30 text-anchor="right" fill="#fff">Latitude</text>
-      <text x=540 y=30 text-anchor="right" fill="#fff">Longitude</text>
+      <text x=200 y=30 text-anchor="right" fill="#fff">Timestamp</text>
+      <text x=350 y=30 text-anchor="right" fill="#fff">Latitude</text>
+      <text x=500 y=30 text-anchor="right" fill="#fff">Longitude</text>
     </g>
     <g  id="wwTasks" text-anchor="right" fill="#000" font-family="Racing Sans One" >
     </g>
