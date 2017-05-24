61e7dff13256d04eeff95a12606e3d2125e3bbd5
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-24 12:16:43 +0100

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -16,12 +16,12 @@
             xlink:href="https://fonts.googleapis.com/css?family=Racing+Sans+One" type="text/css" rel="stylesheet" />
     </defs>
     <g text-anchor="right" font-family="Racing Sans One" >
-      <rect x=0 y=0 width=600 height=50 fill="rgba(59, 120, 231,1)" />
+      <rect x=0 y=0 width=600 height=50 fill="rgba(59, 120, 231,1)" rx=5 />
       <text  x=10 y=30 text-anchor="left"  fill="#fff">ID</text>
       <text  x=60 y=30 text-anchor="right" fill="#fff">Duration seconds</text>
-      <text x=300 y=30 text-anchor="right" fill="#fff">Timestamp</text>
-      <text x=450 y=30 text-anchor="right" fill="#fff">Latitude</text>
-      <text x=600 y=30 text-anchor="right" fill="#fff">Longitude</text>
+      <text x=240 y=30 text-anchor="right" fill="#fff">Timestamp</text>
+      <text x=390 y=30 text-anchor="right" fill="#fff">Latitude</text>
+      <text x=540 y=30 text-anchor="right" fill="#fff">Longitude</text>
     </g>
     <g  id="wwTasks" text-anchor="right" fill="#000" font-family="Racing Sans One" >
     </g>
