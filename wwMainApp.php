406b3c4d174cc297eb1959f09913935da7293e27
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-24 11:34:20 +0100

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -4,27 +4,29 @@
     <meta charset="utf-8">
     <title>WhereWhen :: by Michael Kenneth Rasch :: TRITICUM</title>
     <style>
-      div { float: left}
+      div { width: 100%; margin: 0; padding: 0}
     </style>  
 </head>
-<body style='height: 100%; margin: 0; padding: 0'>
+<body>
+  <div>
   <svg viewBox="0 0 600 300" style="width: 100%; font-family: 'Racing Sans One'"  id="zTasks" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
     <defs>
       <link xmlns="http://www.w3.org/1999/xhtml" href="https://fonts.googleapis.com/css?family=Racing+Sans+One|Six+Caps" type="text/css" rel="stylesheet" />
     </defs>
-    <g text-anchor="right" fill="#fff" font-family="Racing Sans One" >
+    <g text-anchor="right" font-family="Racing Sans One" >
       <rect x=0 y=0 width=600 height=50 fill="rgba(59, 120, 231,1)" />
-      <text x=10 y=0 text-anchor="left">ID</text>
-      <text x=60 y=0 text-anchor="right">Duration seconds</text>
-      <text x=200 y=0 text-anchor="right">Timestamp</text>
-      <text x=300 y=0 text-anchor="right">Latitude</text>
-      <text x=400 y=0 text-anchor="right">Lontitude</text>
+      <text  x=10 y=30 text-anchor="left"  fill="#fff">ID</text>
+      <text  x=60 y=30 text-anchor="right" fill="#fff">Duration seconds</text>
+      <text x=200 y=30 text-anchor="right" fill="#fff">Timestamp</text>
+      <text x=300 y=30 text-anchor="right" fill="#fff">Latitude</text>
+      <text x=400 y=30 text-anchor="right" fill="#fff">Lontitude</text>
     </g>
     <g  id="wwTasks" text-anchor="right" fill="#000" font-family="Racing Sans One" >
     </g>
   </svg>
+  </div>
   <!-- Top part is controls and info - below is google maps with current position marked in a circle -->
-  <div id="wwMap" style='height: 100%; margin: 0; padding: 0'>wwMap</div>
+  <div id="wwMap">wwMap</div>
 </body>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVV112FdOVJA4U5BSwImhPzEI73mgYUjQ" async defer></script>
 <script type="text/javascript" src="staticdata/wwMonitor.js"></script>
