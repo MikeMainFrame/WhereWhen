26658b61a4cea345031b6594f8045b177cc1d8f3
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-28 08:03:50 +0200

    Update wwMonitor.js

--- staticdata/wwMonitor.js
+++ staticdata/wwMonitor.js
@@ -48,7 +48,7 @@ function getStoredData (latLng) {
 }
 function geocodeLatLng(latLng) {
   var geocoder = new google.maps.Geocoder;
-  geocoder.geocode({'location': latLng}, function(results, status) {
+  geocoder.geocode({'location': latLng}, function(results, status)) {
     if (status === 'OK') return results[1].formatted_address;     
 
   }
