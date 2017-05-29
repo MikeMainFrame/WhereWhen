7e43894b37ede79645a658b82e40a2d2c2a84c68
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-05-29 17:03:03 +0200

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -10,22 +10,25 @@
 </head>
 <body>
   <div>
-  <svg viewBox="0 0 600 300" style="width: 100%; font-family: 'Racing Sans One'"  
-       id="zTasks" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <defs>
-      <link xmlns="http://www.w3.org/1999/xhtml" 
-            xlink:href="https://fonts.googleapis.com/css?family=Merriweather" type="text/css" rel="stylesheet" />
-    </defs>
-    <g text-anchor="end" font-family="Merriweather" >
-      <rect x=000 y=00 width=600 height=50 fill="rgba(59, 120, 231,1)" rx=5 />
-      <text x=060 y=30 fill="#fff">ID</text>
-      <text x=240 y=30 fill="#fff">Duration seconds</text>
-      <text x=400 y=30 fill="#fff">Timestamp</text>
-      <text x=500 y=30 fill="#fff">Latitude</text>
-      <text x=600 y=30 fill="#fff">Longitude</text>
-    </g>
-    <g  id="wwTasks" text-anchor="end" fill="#FFF" font-family="Merriweather" ></g>
-  </svg>
+    <svg viewBox="0 0 600 600" style="width: 100%; font-family: 'Racing Sans One'"  
+         id="zTasks" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <defs>
+        <link xmlns="http://www.w3.org/1999/xhtml" 
+              xlink:href="https://fonts.googleapis.com/css?family=Merriweather" type="text/css" rel="stylesheet" />
+      </defs>
+      <g text-anchor="end" font-family="Merriweather" >
+        <rect x=000 y=00 width=600 height=50 fill="rgba(59, 120, 231,1)" rx=5 />
+        <text x=060 y=30 fill="#fff">ID</text>
+        <text x=240 y=30 fill="#fff">Duration seconds</text>
+        <text x=400 y=30 fill="#fff">Timestamp</text>
+        <text x=500 y=30 fill="#fff">Latitude</text>
+        <text x=600 y=30 fill="#fff">Longitude</text>
+      </g>
+      <g  id="wwTasks" text-anchor="end" fill="#FFF" font-family="Merriweather" ></g>
+    </svg>
+    <svg viewBox="0 0 2000 2000" style="width: 100%; font-family: 'Racing Sans One'; visibility: hidden; display: none"  >
+         id="zTimer" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
+    </svg>
   </div>
   <!-- Top part is controls and info - below is google maps with current position marked in a circle -->
   <div id="wwMap">wwMap</div>
