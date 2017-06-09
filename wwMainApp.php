788b93b23f7ffc3ab9ee97769e4cf0ff5aa96259
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-06-09 10:26:06 +0100

    Create wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -8,7 +8,7 @@
 </head>
 <body>
   <div>
-    <svg viewBox="0 0 600 300" style="width: 70% ; float: right"  
+    <svg viewBox="0 0 600 300" style="width: 72% ; float: right"  
          id="zTasks" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
       <defs>
         <link xmlns="http://www.w3.org/1999/xhtml" 
@@ -24,7 +24,7 @@
       </g>
       <g  id="wwTasks" text-anchor="end" fill="#FFF" font-family="Merriweather" ></g>
     </svg>
-    <svg viewBox="0 0 2000 2000" style="width: 29% ; float:left"  
+    <svg viewBox="0 0 2000 2000" style="width: 27% ; float:left"  
          id="zTimer" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
       <rect x=000 y=00 width=2000 height=2000 fill="#FFF" />
     </svg>
