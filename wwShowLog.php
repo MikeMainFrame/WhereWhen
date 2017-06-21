3b7b6559b5615b9d52cc0cac05e4b9ecd47f2df2
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-06-21 08:12:55 +0200

    Create wwShowLog.php

---
+++ wwShowLog.php
@@ -0,0 +1,16 @@
+<?php
+  //header("Content-Type: application/xhtml+xml; charset=utf-8");
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+  $dom = new DOMDocument;
+  $zFileContents =  file_get_contents("gs://wherewhen/XML/wwtask.xml");
+  $dom->loadXML($zFileContents); 
+  $xsl = new DOMDocument;
+  $zFileContents = file_get_contents("gs://crcountlog/crStatic.xsl");
+  $xsl->loadXML($zFileContents); 
+  $proc = new XSLTProcessor;
+  $proc->importStyleSheet($xsl);
+  $clientXML = $proc->transformToDoc($dom);
+      
+  echo $clientXML->saveXML();
+?>
