27ff14cc88d339d2e3ba0e593f431ec04203a0d9
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-06-21 08:01:41 +0200

    Update wwTransaction2.php

--- wwTransaction2.php
+++ wwTransaction2.php
@@ -1,21 +1,23 @@
 <?php
   error_reporting(E_ALL);
   ini_set("display_errors", 1); 
-  //$postdata = file_get_contents("php://input"); 
-  $zFileContents = file_get_contents("gs://wherewhen/XML/wwtask.xml");
-  $mother->loadXML($zFileContents);
+  $mother->loadXML(file_get_contents("gs://wherewhen/XML/wwtask.xml"));
   $mother = new DOMDocument; 
   $root = $mother->documentElement;                    
   
   $transaction = new DOMDocument;                                                // transaction
-  $transaction->loadXML($HTTP_RAW_POST_DATA); 
+  $transaction->loadXML(file_get_contents("php://input")); 
   $id = $transaction->documentElement->getAttribute("id");                       // id of transaction
   $transactionNode = $mother->importNode($transaction->documentElement->firstChild, true);
   
   $domXPath = new DOMXPath($mother); 
   $targetNodes = $domXPath->query("//who[@id = " . $id . "]"); 
   
-  $targetNodes->item(0)->appendChild($transactionNode);
-  file_put_contents("gs://wherewhen/XML/wwtask.xml", $mother->saveXML());
-  echo 'good';
-  ?>
+  if ($targetNodes->length) {
+    $targetNodes->item(0)->appendChild($transactionNode);
+    file_put_contents("gs://wherewhen/XML/wwtask.xml", $mother->saveXML());
+    echo 'good';
+  } else {
+    echo 'mother id not found';
+  }  
+?>
