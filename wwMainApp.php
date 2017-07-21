3c3eb3e1030f9021a716e9a6f840d6f3fa1e6b1e
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-07-21 15:04:39 +0200

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -8,29 +8,8 @@
 </head>
 <body>
   <div id="control">
-    <!--svg viewBox="0 0 600 300" style="width: 100% ; float: right"  
-         id="zTasks" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <defs>
-        <link xmlns="http://www.w3.org/1999/xhtml" 
-              xlink:href="https://fonts.googleapis.com/css?family=Merriweather" type="text/css" rel="stylesheet" />
-       <radialGradient id="blue"  fx="1000" fy="1000" cx="1000" cy="1000" r="1000" gradientUnits="userSpaceOnUse">
-         <stop offset="90%"   stop-color="#0000a0" />
-         <stop offset="95%"   stop-color="#8080ff" />
-         <stop offset="100%"   stop-color="#0000a0" />
-       </radialGradient>    
-     </defs>      
-     <g text-anchor="end" font-family="Merriweather" >
-        <rect x=000 y=00 width=600 height=50 fill="#000" rx=5 />
-        <text x=060 y=30 fill="#fff">ID</text>
-        <text x=160 y=20 fill="#fff">Duration</text>
-        <text x=160 y=40 fill="#fff">Minutes</text>
-        <text x=290 y=30 fill="#fff">Timestamp</text>
-        <text x=300 y=30 fill="#fff" text-anchor="start">Address</text>
-      </g>
-      <g id="wwTasks" text-anchor="end" fill="#FFF" font-family="Merriweather" ></g>
-    </svg-->
     <svg viewBox="0 0 2000 2000"   
-      id="zCanvas" 
+      id="zTask" 
       style="background: #000 ; font-family: 'Racing Sans One'"
       version="1.1" 
       xmlns:xlink="http://www.w3.org/1999/xlink" 
