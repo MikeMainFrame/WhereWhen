bbe63bbc1e5771c21afef3048c550838a090e24c
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-07-29 17:38:32 +0200

    Update wwMonitor.js

--- staticdata/wwMonitor.js
+++ staticdata/wwMonitor.js
@@ -66,8 +66,8 @@ function groupTasks_ShowUI(root, latLng, user) {
       strokeOpacity: 0.8,
       strokeWeight: 2,
       fillColor: "#0000FF",
-      fillOpacity: 0.5,
-      scale: 20
+      fillOpacity: 0.35,
+      scale: 16
       },
     animation: google.maps.Animation.DROP
     });
@@ -81,8 +81,8 @@ function groupTasks_ShowUI(root, latLng, user) {
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: "#FF0000",
-    fillOpacity: 0.5,    
-    scale: 20
+    fillOpacity: 0.35,    
+    scale: 16
     },
   animation: google.maps.Animation.DROP
   });
