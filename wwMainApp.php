dbbb0fc10234181669937885be60219f4a7a4c0d
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-08-27 08:20:27 +0200

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -7,32 +7,18 @@
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Racing+Sans+One|Six+Caps" type="text/css" rel="stylesheet" />    
   </head>
-<body style="background: #000" ; margin: 5px ; color: #fff>
+  <body style="background: #000" ; margin: 5px ; color: #fff>
   <div id="wwControl">
-    <svg viewBox="0 0 2000 2000"   
+    <svg viewBox="0 0 1440 800"   
       id="zControl" 
       style="background: #000 ; font-family: 'Racing Sans One'"
       version="1.1" 
       xmlns:xlink="http://www.w3.org/1999/xlink" 
       xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <path id="insidePath" d="M400,1000, C400,1800 1600,1800 1600,1000" />
-      </defs>
-      <g font-size="64" fill="#f80">
-       <text id="zAddress"    x="10"   y="60">Teglgaardsvej 529</text>
-       <text id="zTimestamp"  x="1990" y="60" text-anchor="end">20170717123456</text>
-       <text id="zTask"       x="10"   y="1960">9999</text>
-       <text id="zAccumulate" x="1990" y="1990" text-anchor="end">23h5m</text>
-      </g>
-      <g>
-       <text font-size="72" font-family="arial" font-weight="600" text-anchor="middle">
-         <textPath id="zId" fill="#aaa" letter-spacing="0.1em" startOffset="50%" xlink:href="#insidePath" opacity="1.0">miketriticum@gmail.com</textPath>
-       </text>  
-      </g>
     </svg>
   </div>
   <!-- Top part is controls and info - below is google maps with current position marked in a circle -->
-  <div style="height: 500px ; width: 100%" id="wwMap">wwMap</div>
+  <div  id="wwMap" style="height: 500px ; width: 100%">wwMap</div>
 </body>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVV112FdOVJA4U5BSwImhPzEI73mgYUjQ&language=da" async defer></script>
 <script type="text/javascript" src="staticdata/wwMonitor.js"></script>
