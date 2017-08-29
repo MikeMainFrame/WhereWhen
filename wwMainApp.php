08625aa5ae12cdebb6e118c0a6c72ae2d594c9c4
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-08-29 16:27:15 +0200

    Update wwMainApp.php

--- wwMainApp.php
+++ wwMainApp.php
@@ -15,6 +15,13 @@
       version="1.1" 
       xmlns:xlink="http://www.w3.org/1999/xlink" 
       xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="0%" style="stop-color:rgb(0,0,255,1)" />
+          <stop offset="50%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+          <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+        </linearGradient>
+      </defs>
     </svg>
   </div>
   <!-- Top part is controls and info - below is google maps with current position marked in a circle -->
