bf9c6273705603e0c2120aaeda055ee8a63761ab
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-11-14 12:33:45 +0100

    Update postTime.js

--- staticdata/postTime.js
+++ staticdata/postTime.js
@@ -2,5 +2,5 @@ hook = document.querySelectorAll(".timestamp");
 for (ix = 1; ix < hook.length; ix++) {  
   var aTime = new Date(parseInt(hook[ix].textContent));
   temp = new Date(parseInt(hook[ix].textContent))
-  hook[ix].textContent = temp.d.toUTCString();
+  hook[ix].textContent = temp.toUTCString();
 }
