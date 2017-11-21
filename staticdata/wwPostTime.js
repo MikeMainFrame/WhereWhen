00fd2c42f1d54e38fd3102089b8a711ab512d188
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-11-21 11:28:37 +0100

    Update wwPostTime.js

--- staticdata/wwPostTime.js
+++ staticdata/wwPostTime.js
@@ -2,5 +2,12 @@ hook = document.querySelectorAll(".timestamp");
 for (ix = 1; ix < hook.length; ix++) {  
   var aTime = new Date(parseInt(hook[ix].textContent));
   temp = new Date(parseInt(hook[ix].textContent))
-  hook[ix].textContent = temp.toUTCString();
+  hook[ix].textContent = convertDateToUTC(temp);
+}
+function convertDateToUTC(date) { 
+  return parseInt((date.getFullYear() * 1.0e+08) 
+                  + ((date.getMonth() + 1) * 1.0e+06) 
+                  + (date.getDate() * 1.0e+4) 
+                  + (date.getHours() * 100)
+                  + date.getMinutes());
 }
