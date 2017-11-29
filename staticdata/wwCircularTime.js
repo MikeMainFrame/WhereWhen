499ca4d65a58e33a3b9f87791baa58b55e7390a6
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-11-29 13:38:58 +0100

    Update wwCircularTime.js

--- staticdata/wwCircularTime.js
+++ staticdata/wwCircularTime.js
@@ -31,10 +31,12 @@
     var g = document.createElementNS("http://www.w3.org/2000/svg", 'g');    
 
     for (ix = 0; ix < slices.length; ix++) {
+      
       zMinutes = slices[ix].duration * 360 / zBand;        
       zOffset = slices[ix].timestamp - min;
       var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');    
       path.setAttribute("id", ix);     
+      path.setAttribute("ztimestamp", slices[ix].timestamp); 
       path.setAttribute("fill", thisColor); 
       path.setAttribute("stroke", thisColor); 
       path.setAttribute("stroke-width", 0);  
@@ -45,8 +47,24 @@
         + "L " + parseFloat(500 + (Math.cos((zMinutes + zOffset) * Math.PI/180) * iRadius)) + ", " + parseFloat(500 - (Math.sin((zMinutes + zOffset) * Math.PI/180) * iRadius))
         + "A " + iRadius + "," + iRadius + " 1 0,0 " +  parseFloat(500 + (Math.cos((zMinutes + zOffset) * Math.PI/180) * iRadius)) +  "," + parseFloat(500 - (Math.sin((zMinutes + zOffset) * Math.PI/180) * iRadius))
         + " Z");        
-      g.appendChild(path);                  
+      path.addEventListener("click", showInfo);
+      g.appendChild(path);
+      
+      var today = new Date(slices[ix].timestamp); 
+      var text = document.createElementNS("http://www.w3.org/2000/svg", 'text');    
+      text.setAttribute("font-family", "Barlow Condensed");
+      text.setAttribute("font-size", 24);
+      text.setAttribute("fill", "red");
+      text.setAttribute("transform", "rotate(" + parseInt(zMinutes + zOffset) + " 500 500)");
+      text.setAttribute("x",  parseFloat(500 + (Math.cos(zOffset * Math.PI/180) * oRadius)));     
+      text.setAttribute("y",  parseFloat(500 - (Math.sin(zOffset * Math.PI/180) * oRadius)));     
+      text.setAttribute("font-family", "Barlow Condensed");
+      text.textContent = today.toUTCString();
+      g.appendChild(text);
     }        
     return(g);
+  }
+  function showInfo(what) {
+    alert("clicked this slice: " + what.getAttribute("ztimestamp"); 
   }
 })("miketriticum@gmail.com"); 
