71a23c48e2f2e1a92ce23c5fc37d9742cd9f8cb6
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-12-01 09:00:19 +0100

    Update wwCircularTime.js

--- staticdata/wwCircularTime.js
+++ staticdata/wwCircularTime.js
@@ -29,17 +29,20 @@
 
     var zBand = max - min;
     var zUnits = zBand / 360;        
-    var g = document.createElementNS("http://www.w3.org/2000/svg", 'g');    
+    var g = document.createElementNS("http://www.w3.org/2000/svg", 'g');  
+    g.setAttribute("text-anchor", "middle");
+    g.setAttribute("font-family", "Barlow Condensed");
+    g.setAttribute("font-size", 18);      
+    g.setAttribute("fill", "white");
 
-    for (ix = 0; ix < slices.length; ix++) {
-      
+    for (ix = 0; ix < slices.length; ix++) {      
       zMinutes = slices[ix].duration / zUnits;
       zOffset = (slices[ix].timestamp - min) / zUnits;
       var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');    
       path.setAttribute("id", ix);     
       path.setAttribute("ztimestamp", slices[ix].timestamp); 
-      path.setAttribute("fill", thisColor); 
-      path.setAttribute("stroke", thisColor); 
+      path.setAttribute("zduration", slices[ix].duration); 
+      path.setAttribute("fill", thisColor);      
       path.setAttribute("stroke-width", 0);  
       path.setAttribute("d",
           "M " + parseFloat(500 + (Math.cos(zOffset * Math.PI/180) * iRadius)) + ", " + parseFloat(500 - (Math.sin(zOffset * Math.PI/180) * iRadius))
@@ -55,14 +58,10 @@
       var text = document.createElementNS("http://www.w3.org/2000/svg", 'text');    
       X = parseFloat(350 + (Math.cos(zOffset * Math.PI/180) * oRadius));
       Y = parseFloat(350 - (Math.sin(zOffset * Math.PI/180) * oRadius));     
-      text.setAttribute("text-anchor", "middle");
-      text.setAttribute("font-family", "Barlow Condensed");
-      text.setAttribute("font-size", 16);
-      text.setAttribute("fill", "white");
-      text.setAttribute("transform", "rotate(" + parseInt(zMinutes + zOffset) + " " + X + "," + Y + ")");
+      
       text.setAttribute("x",  X);     
       text.setAttribute("y",  Y);     
-      text.setAttribute("font-family", "'Barlow Condensed', sans-serif");
+      
       text.textContent = slices[ix].duration;
       g.appendChild(text);
     }        
