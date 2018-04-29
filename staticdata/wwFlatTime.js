746c27a333cd5218e18dd88f765b89e4aec0486a
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-04-29 07:16:14 +0200

    Update wwFlatTime.js

--- staticdata/wwFlatTime.js
+++ staticdata/wwFlatTime.js
@@ -34,7 +34,7 @@
       zDate = new Date(slices[ix].timestamp);
       zSum = zSum + parseInt(slices[ix].duration);
       x = (max - slices[ix].timestamp) / 86400000 / xUnits;
-      y = (880 - (zDate.getHours() * 60) + zDate.getMinutes * 60)) / yUnits;
+      y = (880 - (zDate.getHours() * 60) + (zDate.getMinutes * 60) / yUnits;
       var rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');    
       rect.setAttribute("id", ix);     
       rect.setAttribute("ztimestamp", slices[ix].timestamp); 
